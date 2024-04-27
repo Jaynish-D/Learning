f5f935292280cb0a92cdf53e38a4c3d6f57a775c
Jaynish Chauhan <19it191jd@gmail.com>
2024-04-27 17:03:43 +0530

    Project Push.

---
+++ Learning/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Learning
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Swagger/OpenAPI configuration
+            services.AddEndpointsApiExplorer();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            //});
+
+            // Cors Policy
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("CorsPolicy",
+            //        builder => builder
+            //            .AllowAnyOrigin()
+            //            .AllowAnyMethod()
+            //            .AllowAnyHeader());
+            //});
+
+            //services.AddDbContext<ApplicationDBContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                // Swagger/OpenAPI setup
+                //app.UseSwagger();
+                //app.UseSwaggerUI();
+            }
+
+            //app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
