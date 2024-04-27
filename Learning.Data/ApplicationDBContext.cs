92afc43ff04f56fbb7e2c0b04bbcd5740e3c57cd
Jaynish Chauhan <19it191jd@gmail.com>
2024-04-27 19:54:07 +0530

    L-01: Insalled EntityFrameworkCore Packages.

---
+++ Learning.Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Learning.Data.UserEntity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learning.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :
+        base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
